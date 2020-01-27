3f9e324f48f73d18ae70da0d12f610fe96a8b961
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2020-01-27 18:30:00 -0500

    refactored stack

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -16,57 +16,29 @@ class Stack:
         self.size = 0
         self.top_item = None
         # Why is our DLL a good choice to store our elements?
-        self.storage = 1000 #this is the limit
+        self.storage = DoublyLinkedList()
 
-    def push(self, value):
+    def push(self, value):        
 
-        #if there is still space on the stack
-        if self.len() < self.storage:
+        self.storage.add_to_head(value)
 
-            #instantiate a node with value as an argument and assign it to the variable new_item
-            #because this item is a node, we have easy access to ListNode’s class methods
-            new_item = ListNode(value)
+        #increment the size of the stack
+        self.size += 1     
 
-            #increment the size of the stack
-            self.size += 1
-
-            #if the stack is empty
-            if not self.top_item:
-
-                #set the new node as the top item
-                self.top_item = new_item
-
-            #if the stack is not empty
-            else:
-                #set the existing top item's prev pointer to new_item
-                self.top_item.prev = new_item 
-
-                #set the new_item's next pointer to the current top item
-                new_item.next = self.top_item
-
-                #set the new_item as the top item
-                self.top_item = new_item 
-
-        #if there is no space left on the stack
-        else:
-            print("All out of space!")
+        
         
     def pop(self):
 
         #if the stack is not empty
         if self.len() > 0:
 
-            #always remove the first item from a stack first
-            item_to_remove = self.top_item
-
-            #this removes the self.top_item's next pointer (assigns it back to itself)
-            self.top_item = item_to_remove.next
+            value = self.storage.remove_from_head()
 
             #decrement the stack size
             self.size -= 1
 
             #return's the value of item_to_remove after the item is popped or removed
-            return item_to_remove.value
+            return value
         
         #if the stack is not empty
         else:
