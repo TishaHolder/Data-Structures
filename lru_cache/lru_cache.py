34c9c4d455dbe6529862d3179a89331cdce40466
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2020-01-28 20:32:23 -0500

    in progress

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -9,10 +9,31 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
+    """when we have a cache we want to be able to look something up by a certain key. we can use a key 
+       to look in the cache and see if the data corresponding to that key is in there"""
+
+    """when adding items to the cache/dictionary by key we also have to add it to the linked list at the head
+       to indicate it is the most recently used (MRU) item"""
+
+    """to get items from the cache we look up in the dictionary by key to see if the item is there.
+       if it is there we need to mark the item as most recently used (MRU) by moving it to the 
+       head of the list"""
+
+    """if the cache is over full or at max capacity, look at the least recently used (LRU) item 
+       at the end of the list (tail) and delete it from both the list and the dictionary"""
+
     def __init__(self, limit=10):
         self.limit = limit
-        self.size = None
+        self.size = 0
+
+        #used to store or order the data in memory and organize the data in a 
+        # most recently used/least recently used way
         self.list = DoublyLinkedList()
+
+        #dictionary used as cache to store key value pairs and point to every element in the DoublyLinkedList
+        #if we want a pointer to Node C, use dictionary to look up by the key of C and it will return 
+        #a pointer to C
         self.storage = {}
 
 
@@ -24,22 +45,20 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        for i in self.storage:
-            if key in key.storage:
+                
+            if key in self.storage:
 
-                #if the key is found store the associated value in the value variable                
+                #Retrieves the value associated with the given key.                
                 value = self.storage[key]
 
-                #this removes the key/value pair specified by the key from the dict. it returns the removed value
-                self.storage.pop(key)
+                #Also needs to move the key-value pair to the end of the order such that the pair is 
+                # considered most-recently used.
+                self.list.move_to_front(value)                
 
-                #use the set method below to move the pair to the end of the order so it's MRU
-                self.set(key, value)
-
-                #rerturns the value associated with the given key
+                #Returns the value associated with the key
                 return self.storage[key]
             else:
-                #returns none of the key/value pair doesn't exist
+                #returns none if the key/value pair doesn't exist
                 return None
 
 
@@ -55,4 +74,37 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+
+        #checks if the cache is already at max capacity before this entry is added
+        if self.size < self.limit:
+
+            #decrement size of cache or dictionary by 1
+            self.size += 1
+
+            #then check if key already exists
+            # if it does, overwrite the old value associated with the key with the new value
+            if self.storage[key] == key:
+                self.storage[key] = value            
+           
+            #then retrieve the given key/value pair from the dictionary
+            new_item = self.storage.get(key)
+
+            #and add it to the doubly linked list as the first item (MRU)
+            self.list.add_to_head(new_item)            
+
+            #increment the size of the cache
+            self.size += 1
+        #if the cache is at max capacity, delete the oldest entry in the cache to make room
+        else:          
+
+            #remove it from the cache or dictionary
+            del self.storage[self.list.tail.value[0]]
+
+            #remove it from the doubly linked list
+            self.list.remove_from_tail()
+
+            self.size -= 1
+
+            
+
+
